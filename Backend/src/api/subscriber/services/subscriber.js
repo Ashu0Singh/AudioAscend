61c50a161669e3c9853a27d71bbca8e5f2512d49
Ashu0Singh <ashusingh1417@gmail.com>
2023-07-11 21:41:16 +0530

    Updates

--- Backend/src/api/subscriber/services/subscriber.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * subscriber service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::subscriber.subscriber');
